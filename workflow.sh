2b2ce13c90e3e98af0a84d9d059580a693ccc45a
Brandon Corey <93304067+BrandonCorey@users.noreply.github.com>
2023-03-07 20:54:01 -0500

    Create workflow.sh

---
+++ workflow.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Initialize npm package
+npm init
+
+# Install dependencies and dev-dependencies
+npm install express --save
+npm install pug --save
+npm install nodemon --save-dev
+npm install eslint eslint-cli babel-eslint --save-dev
+
+# Add script for `npm start` that executes `nodemon` for program
+echo "\"scripts\": {
+    \"start\": \"nodemon <main_program_name>\"
+  }," >> package.json
+
+# Create views directory for pug files
+mkdir views
+
+# Create layout pug file for any boilerplate to be included in all files
+touch views/layout.pug
+
+# Create includes directory for any pug files you want to import to other pug files
+mkdir views/includes
+
+# Create public directory for static assets
+mkdir public
+
+# Create stylesheets directory for external CSS
+mkdir public/stylesheets
+
+# Output instructions for running the app
+echo "Your app is ready! To start, run: npm start"
